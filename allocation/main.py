fa0cf6c35c21d97e82ca50e8c704515bbe3e771c
Matías Graña <matias.alejo@gmail.com>
2019-01-06 16:06:46 -0300

    code style & mypy in main

--- allocation/main.py
+++ allocation/main.py
@@ -2,16 +2,19 @@ from argparse import ArgumentParser
 import logging
 import yaml
 
-from .allocating import WeightedMap, Allocator, Allocation
+from .allocating import WeightedMap, WeightedNode, Allocator, Allocation
 
 logger = logging.getLogger(__name__)
 
 
 def parse():
     parser = ArgumentParser()
-    parser.add_argument('-a', '--allocate', help='yaml file with resources to allocate', required=True)
-    parser.add_argument('--out', choices=('empty', 'term'), default='term', help='type of output')
-    parser.add_argument('-v', '--verbose', action='count', default=0, help='be (more) verbose')
+    parser.add_argument('-a', '--allocate', required=True,
+                        help='yaml file with resources to allocate')
+    parser.add_argument('--out', choices=('empty', 'term'), default='term',
+                        help='type of output')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='be (more) verbose')
     args = parser.parse_args()
     return args
 
@@ -25,7 +28,8 @@ def load_yaml(infile) -> Allocator:
         with open(infile) as yafile:
             y = yaml.load(yafile)
 
-    wmap_list = [{'from': w[0], 'to': w[1], 'weight': w[2]} for w in y['weights']]
+    wmap_list = [WeightedNode({'from': w[0], 'to': w[1], 'weight': w[2]})
+                 for w in y['weights']]
     wmap = WeightedMap(wmap_list)
 
     return Allocator(y['sources'], wmap, y['targets'])
