40315958e51a74774f176a634606a82886025233
Matías Graña <matias.alejo@gmail.com>
2019-02-16 19:17:18 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='allocation',
+      version='0.1',
+      description='A package to allocate resources',
+      author='Matías Graña',
+      author_email='matias.alejo@gmail.com',
+      py_modules=['allocation'],
+      )
