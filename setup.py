4fccc0b5e18129d85317776635ce392472d5237e
Matías Graña <matias.alejo@gmail.com>
2019-03-11 01:36:39 -0300

    update version to 0.9 as it is close to be a working lib.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='allocation',
-      version='0.1',
+      version='0.9',
       description='A package to allocate resources',
       author='Matías Graña',
       author_email='matias.alejo@gmail.com',
