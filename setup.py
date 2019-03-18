1b73291fceae84b365fb394b9dfe7b124c6b1a59
Matías Graña <matias.alejo@gmail.com>
2019-03-18 01:34:18 -0300

    version++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='allocation',
-      version='0.10',
+      version='0.11',
       description='A package to allocate resources',
       author='Matías Graña',
       author_email='matias.alejo@gmail.com',
